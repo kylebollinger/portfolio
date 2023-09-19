91eba3bf3616eebd6a8133fc3420a02438086386
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-19 12:16:58 -0700

    Setup initial postgres db config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -64,9 +64,13 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", "kport"),
+        'USER': os.environ.get("DB_USER", "kboll"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST", "127.0.0.1"),
+        'PORT': os.environ.get("DB_PORT", "5432"),
     }
 }
 

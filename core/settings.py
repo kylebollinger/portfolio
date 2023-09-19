a02acb8f93db8f645e932a9705075494f0691e88
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-19 14:24:20 -0700

    Setup a base app.html template to inherit from

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,9 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'main/templates')
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

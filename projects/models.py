6b99b34cb05aa155cdf3b9fb379fb7348c764ae1
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-19 16:49:09 -0700

    Create migration for Tag model and register in admin panel

--- projects/models.py
+++ projects/models.py
@@ -10,11 +10,30 @@ class Project(models.Model):
     year = models.IntegerField(null=True, blank=True)
     slug = models.SlugField(max_length=128, unique=True, blank=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     data = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
         if self.title and len(self.title) > 0:
             self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=128, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.name and len(self.name) > 0:
+            self.name = self.name.lower().strip()
+            self.slug = slugify(self.name)
         super().save(*args, **kwargs)

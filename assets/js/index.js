4e560934dc55c69e1dacd90aea96bc755d45bd1a
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-21 09:02:36 -0700

    Import bootstrap into src assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
 const globalMenu = document.querySelector('[data-global-nav-btn]');
 globalMenu.addEventListener('click', event => {
     globalMenu.classList.toggle('active');

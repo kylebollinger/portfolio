31417726840254c931503b864e63139d51fc5b5c
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-22 08:22:03 -0700

    Update navbar logo to gradient fill and disable burger menu for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,2 @@
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
-
-const globalMenu = document.querySelector('[data-global-nav-btn]');
-globalMenu.addEventListener('click', event => {
-    globalMenu.classList.toggle('active');
-});

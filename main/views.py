9b998313077627d372325dc19dc1c37c2a0d4df2
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-22 11:11:08 -0700

    Add projects to home view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'main/home.html', context)

be7ef3eb359128863aa61da43db1bbebf48047df
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-22 09:10:14 -0700

    Move templates to root project folder

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'main/templates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         "APP_DIRS": True,
         "OPTIONS": {
